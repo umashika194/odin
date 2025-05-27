cae44af4d5c63c3247262ce863adb9354aa85fcd
sneaks0n <gitea@umashika.de>
2025-05-27 23:20:04 +0200

    object excercises

---
+++ JAVA/basics_test/object_excercises.js
@@ -0,0 +1,35 @@
+// https://javascript.info/object
+// Hello, object
+
+let user = {};
+user["name"] = "john";
+user["surname"] = "smith";
+user.name = "pete";
+delete user.name;
+
+// Check for emptiness
+
+let schedule = {};
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    // if the loop has started, there is a property
+    return false;
+  }
+  return true;
+}
+
+// oder 
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false
+
+//
