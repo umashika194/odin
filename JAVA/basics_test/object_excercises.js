bbe9ba3dd01af750c34ecce51304207ccb98a500
sneaks0n <sneaks0n@phoenix.de>
2025-05-28 16:15:08 +0200

    further obj excerices

--- JAVA/basics_test/object_excercises.js
+++ JAVA/basics_test/object_excercises.js
@@ -33,3 +33,50 @@ schedule["8:30"] = "get up";
 alert( isEmpty(schedule) ); // false
 
 // Sum object properties
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+  Schmete: 1000
+}
+
+let sum = 0;
+function sumobj(obj) {
+  for (let prop in obj) {
+    key = prop;
+    sum += obj[prop];
+  }
+return sum;
+}
+
+sumobj(salaries);
+console.log(sum);
+
+// Multiply numeric property values by 2
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+
+function multiplyobj(obj) {
+  for (let prop in obj) {
+    console.log(typeof obj[prop]);
+    if (typeof obj[prop] === "number") {
+      obj[prop] = (obj[prop] * 2);
+    }
+  }
+}
+
+multiplyobj(menu);
+console.log(menu);
